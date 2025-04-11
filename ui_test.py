c8c7b2e46d24f8b7146f1c262a7d13d6ced01fd3
Alexey-Nikolaevic-B <alexey.bardushko@gmail.com>
2025-04-11 13:06:01 +0300

    #fixed timer

--- ui_test.py
+++ ui_test.py
@@ -26,7 +26,7 @@ class TestScreen(QDialog):
         super(TestScreen, self).__init__()
         
         self.test_path = " "
-        self.cur_task = 300
+        self.cur_task = 0
         self.image  = ["empty", "img\map.jpg"]
         self.tasks  = ["Английский перевод 1914 года, H. RackhamOn the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains", "Английский перевод 1914 года, H. RackhamOn the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this principle of selection: he rejects pleasures to secure other greater pleasures, or else he endures pains to avoid worse pains"]
         self.checks = ["1", "2"]
@@ -87,6 +87,7 @@ class TestScreen(QDialog):
             self.lbl_time.setText(str(timer))
 
             time.sleep(1)
+        self.endTest()
 
     def set_path(self, path):
         self.test_path = path
